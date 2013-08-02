dfc09edb5b07b39a09dee4e34e8a1ca3a7ea84e4
Carter Bancroft <alt234@Carter-Bancrofts-MacBook-Pro-5.local>
2013-08-02 09:36:56 -0800

    Add initial game... Spinning block.

---
+++ game.rb
@@ -0,0 +1,64 @@
+require 'rubygems'
+$LOAD_PATH.unshift File.join(File.expand_path(__FILE__), "..", "..", "lib")
+require 'chingu'
+include Gosu
+include Chingu
+
+class Game < Chingu::Window
+    def initialize
+        super
+        retrofy
+        @factor = 3
+        @input = { :escape => :exit }
+        #@cursor = false
+        @cursor = Gosu::Image.new(self, 'media/mouse.png')
+        @block = Block.create(:x => $window.width/2, :y => $window.height/2)
+    end
+
+    def draw
+        super
+        @cursor.draw(self.mouse_x, self.mouse_y, 100)
+    end
+
+    def needs_cursor?
+        false
+    end
+end
+
+class Block < Chingu::GameObject
+    traits :timer
+    
+    def initialize(options = {})
+        super
+
+        self.input = [:released_left_mouse_button]
+
+        @animation = Chingu::Animation.new(:file => "media/sheet_25x30.bmp")
+        @animation.frame_names = { :spin => 0..4 }
+        @image = @animation.first
+        @spin = false
+        update
+    end
+
+    def released_left_mouse_button
+        if $window.mouse_x >= (self.x - self.width/2) && $window.mouse_x <= (self.x + self.width/2) &&
+            $window.mouse_y >= (self.y - self.height/2) && $window.mouse_y <= (self.y + self.height/2)
+            @spin = true
+        end
+    end
+
+    def update
+        if @spin
+            @image = @animation[:spin].next
+        #else
+         #   @image = @animation.first
+        end
+        
+        if @image == @animation.last
+            @spin = false
+            @image = @animation.first
+        end
+    end
+end
+
+Game.new.show
